e68cd80a538b32ec2cff953afcabf6c789016285
Uday Nayak <udaynayakgkv@gmail.com>
2020-03-26 17:07:14 +0530

    product catalog and relevant styling added

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :title, length: { minimum:10,
+                              message: 'too short' }
 end
