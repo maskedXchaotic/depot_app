9e106b6a33e885aac67c39bb360ec65e97ecb4bf
maskedxchaotic <udaynayakgkv@gmail.com>
2020-04-08 11:45:55 +0530

    line items validations

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,8 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :product
   belongs_to :cart, optional: true
+  validates_uniqueness_of :cart, scope: :product
+
 
   def total_price
     product.price * quantity
