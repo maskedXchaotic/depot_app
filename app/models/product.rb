c2bd9ef47d4f1c31dc313bfc384687c4bdaaf105
maskedxchaotic <udaynayakgkv@gmail.com>
2020-04-08 11:40:43 +0530

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,8 +15,13 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :title, length: {minimum: 10}
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }, allow_blank: true
+  validates :permalink, uniqueness: true, format: {with: /\A[a-zA-Z0-9 ]+\z/}, length: {minimum:3}
+  validate :description_word_check
+  validates :image_url, url:true
 
+  validates :price, numericality: {greater_than_or_equal_to: :discount_price}
+  # validate :price_greater_than_discount_price
 
   private
 
@@ -27,4 +32,22 @@ class Product < ApplicationRecord
         throw :abort
       end
     end
+
+    def description_word_check
+      word_length = description.split.length
+      if word_length < 5
+        errors.add(:base, 'Descrription Word length must be greater than 5')
+      end
+
+      if word_length > 10
+        errors.add(:base, 'Descrription Word length cannot be greater than 10')
+      end
+    end
+
+    def price_greater_than_discount_price
+      if price < discount_price
+        errors.add(:base, 'Price should be greater than or equal to discount price')
+      end
+    end
+
 end
