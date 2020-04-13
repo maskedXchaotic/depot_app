22b8ed6e65fe350a5fb62dde9b6283451d650e8c
maskedxchaotic <udaynayakgkv@gmail.com>
2020-04-13 15:38:37 +0530

    made changes after review

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness:true
   has_secure_password
   after_destroy :enusre_an_admin_remains
-  validates :email, uniqueness: {case_sensitive: false}, format: {with: /.+@.+\..+/}
-  has_many :orders
+  validates :email, uniqueness: {case_sensitive: false},g format: {with: /.+@.+\..+/}
+  has_many :orders, dependent: :restrict_with_error
 
   class Error < StandardError
   end
---
+++ db/migrate/20200413100615_add_index_to_orders.rb
@@ -0,0 +1,6 @@
+class AddPartNumberToProducts < ActiveRecord::Migration[6.0]
+  def change
+    add_index :orders, :user_id
+    add_foreign_key :orders, :users
+  end
+end
