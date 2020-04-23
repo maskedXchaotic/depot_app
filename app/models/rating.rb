d8839e7651febeb4d41f3b29d82703c49f74ef4e
Uday Nayak <udaynayakgkv@gmail.com>
2020-04-23 16:28:17 +0530

    changes

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ApplicationRecord
  belongs_to :product
- validates :value, numericality: {min:1 max:5}
- validates :user_id, unqiueness: {scope: :product_id}
+ validates :value, numericality: {min: 1, max: 5}
+ validates :user_id, uniqueness: {scope: :product_id}
 end
